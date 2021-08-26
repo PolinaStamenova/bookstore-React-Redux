b98abff98b0b02185fba61f8f67693036e8b0475
PolinaStamenova <polina.stamenova@gmail.com>
2021-08-26 21:17:36 +0300

    Add styling to Nav component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,38 +1,42 @@
-/* eslint-disable jsx-quotes, quotes, object-curly-newline */
-import React from "react";
-import { BrowserRouter as Router, Switch, Link, Route } from "react-router-dom";
-import PageBooks from "./books/PageBooks";
-import Categories from "./categories/Categories";
-import "./Nav.css";
+import React from 'react';
+import {
+  BrowserRouter as Router, Switch, Link, Route,
+} from 'react-router-dom';
+import PageBooks from './books/PageBooks';
+import Categories from './categories/Categories';
+import './Nav.css';
+import './categories/Categories.css';
 
 export default function Nav() {
   return (
     <Router>
-      <nav>
-        <div className='nav-bar'>
-          <h1 className='nav-title'>Bookstore CMS</h1>
-          <ul>
-            <li>
-              <Link to='/' className='nav-books'>
-                BOOKS
-              </Link>
-            </li>
-            <li>
-              <Link to='/categories' className='nav-categories'>
-                CATEROGRIES
-              </Link>
-            </li>
-          </ul>
-        </div>
-        <div className='user-icon-wrapper'>
-          <i className='fas fa-user-alt' />
-        </div>
-      </nav>
+      <header>
+        <nav>
+          <div className="nav-bar">
+            <h1 className="nav-title">Bookstore CMS</h1>
+            <ul>
+              <li>
+                <Link to="/" className="nav-books">
+                  BOOKS
+                </Link>
+              </li>
+              <li>
+                <Link to="/categories" className="nav-categories">
+                  CATEROGRIES
+                </Link>
+              </li>
+            </ul>
+          </div>
+          <div className="user-icon-wrapper">
+            <i className="fas fa-user-alt" />
+          </div>
+        </nav>
+      </header>
       <Switch>
-        <Route path='/categories'>
+        <Route path="/categories">
           <Categories />
         </Route>
-        <Route path='/'>
+        <Route path="/">
           <PageBooks />
         </Route>
       </Switch>
